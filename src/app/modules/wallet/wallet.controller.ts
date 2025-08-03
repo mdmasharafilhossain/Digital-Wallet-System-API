7e7fd67af7eb3f89d890dd1c28c5efb0a745d5ca
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-08-03 21:15:13 +0600

    wallet controller added

---
+++ src/app/modules/wallet/wallet.controller.ts
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../../utils/appError";
+import * as walletService from "./wallet.service";
+import { addMoneySchema, cashInOutSchema, sendMoneySchema } from "./wallet.schema";
+import mongoose from "mongoose";
+
+
+export const addMoney = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { amount } = addMoneySchema.parse(req.body);
+    const wallet = await walletService.addMoney(req.user._id, amount);
+    
+    res.status(200).json({
+      status: "success",
+      data: { wallet }
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+export const sendMoney = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { receiverId, amount } = sendMoneySchema.parse(req.body);
+    await walletService.sendMoney(
+      req.user._id,
+      new mongoose.Types.ObjectId(receiverId),
+      amount
+    );
+    
+    res.status(200).json({
+      status: "success",
+      message: "Money sent successfully"
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+export const cashIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { userId, amount } = cashInOutSchema.parse(req.body);
+    await walletService.cashIn(
+      req.user._id,
+      new mongoose.Types.ObjectId(userId),
+      amount
+    );
+    
+    res.status(200).json({
+      status: "success",
+      message: "Cash-in successful"
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+export const cashOut = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { userId, amount } = cashInOutSchema.parse(req.body);
+    await walletService.cashOut(
+      req.user._id,
+      new mongoose.Types.ObjectId(userId),
+      amount
+    );
+    
+    res.status(200).json({
+      status: "success",
+      message: "Cash-out successful"
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+export const getWallet = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const wallet = await walletService.getWallet(req.user._id);
+    res.status(200).json({
+      status: "success",
+      data: { wallet }
+    });
+  } catch (err: any) {
+    next(new AppError(404, "Wallet not found"));
+  }
+};
